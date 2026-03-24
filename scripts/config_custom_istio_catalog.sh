7d1892bb84172fd3696a4182e3b2ab28d10b9c2d
Surender Yadav <surender.yadav1@ibm.com>
2026-03-24 02:46:44 -0400

    Update istio catalog script and add skip subsuite

--- scripts/config_custom_istio_catalog.sh
+++ scripts/config_custom_istio_catalog.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 if ! oc whoami &>/dev/null; then
   echo "You are not logged into an OpenShift cluster."
   echo "Please log in using: oc login -u kubeadmin -p <password> --server=https://api.clustername.maistra.upshift.redhat.com:6443 --insecure-skip-tls-verify"
+  echo ""
   exit 1
 fi
 
@@ -14,12 +15,27 @@ echo "Current cluster: $(oc whoami --show-server)"
 
 SOURCE_ROOT="$(pwd)"
 
-if oc get catalogsource custom-istio-catalog -n openshift-marketplace &>/dev/null; then
-    echo "Catalog custom-istio-catalog already exists"
+CATALOG_NS="openshift-marketplace"
+CATALOG_NAME="custom-istio-catalog"
+
+if oc get catalogsource "$CATALOG_NAME" -n "$CATALOG_NS" &>/dev/null; then
+    echo "CatalogSource $CATALOG_NAME already exists"
+
+    read -rp "Do you want to delete and recreate it? (Y/N): " DEL
+    [[ ! "$DEL" =~ ^[Yy]$ ]] && echo "Skipping CatalogSource operation" && exit 0
+
+    echo "Deleting existing CatalogSource..."
+    oc delete catalogsource "$CATALOG_NAME" -n "$CATALOG_NS" --wait=true
+
 else
+    echo "CatalogSource $CATALOG_NAME does NOT exist"
+
+    read -rp "Do you want to create it? (Y/N): " CREATE
+    [[ ! "$CREATE" =~ ^[Yy]$ ]] && echo "Skipping CatalogSource creation" && exit 0
+fi
 
 while true; do
-    read -p "Enter Image IIB only: " IIB
+    read -rp "Enter Image IIB only: " IIB
     [[ -n "$IIB" ]] && break
     echo "IIB cannot be empty"
 done
@@ -28,11 +44,26 @@ IMAGE="brew.registry.redhat.io/rh-osbs/iib:$IIB"
 
 CUSTOM_CATALOG_FILE="$SOURCE_ROOT/istio/jenkins-csb-declaration/resources/ocp/templates/olm/custom/custom-catalog-source.yaml"
 
-yq -i '
-  .metadata.name = "custom-istio-catalog" |
-  .spec.displayName = "RH Custom Istio catalog" |
-  .spec.image = "'$IMAGE'"
-' "$CUSTOM_CATALOG_FILE"
+yq -i "
+  .metadata.name = \"$CATALOG_NAME\" |
+  .spec.displayName = \"RH Custom Istio catalog\" |
+  .spec.image = \"$IMAGE\"
+" "$CUSTOM_CATALOG_FILE"
 
+echo "Applying CatalogSource..."
 oc apply -f "$CUSTOM_CATALOG_FILE"
+
+echo "Waiting for CatalogSource to be READY..."
+
+oc wait catalogsource/custom-istio-catalog \
+  -n openshift-marketplace \
+  --for=jsonpath='{.status.connectionState.lastObservedState}'=READY \
+  --timeout=180s
+
+if [[ $? -ne 0 ]]; then
+    echo " CatalogSource did not become READY"
+    oc get catalogsource custom-istio-catalog -n openshift-marketplace -o yaml | grep -A5 connectionState
+    exit 1
 fi
+
+echo "CatalogSource is ready"
--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -99,7 +99,7 @@ fi
 if [ "${TEST_TYPE}" = "single_test" ]; then
      read -rp "Enter Package from ($ALLOWED) or Sub-package: " PACKAGE
      #read -rp "Enter Tests to skip: " SKIP_TESTS
-     #read -rp "Enter Sub-package to skip: " SKIP_SUBSUITES
+     read -rp "Enter Sub-package to skip: " SKIP_SUBSUITES
      read -rp "Enter Test Name: " RUN_TEST_ONLY
 else
 
@@ -184,7 +184,7 @@ TEST_NAME="${TEST_PACKAGE//\//_}"
       RUN_ALL=""
       SKIP_PARSER_SUITE="${TEST_PACKAGE}"
       SKIP_PARSER_SKIP_TESTS=""
-      SKIP_PARSER_SKIP_SUBSUITES=""
+      SKIP_PARSER_SKIP_SUBSUITES="${SKIP_SUBSUITES}"
       SKIP_PARSER_RUN_TESTS_ONLY="${RUN_TEST_ONLY}"
       export ARTIFACT_DIR="/root/artifacts_istio/${RELEASE_VERSION}_single_test/${TEST_PACKAGE}/${TEST_NAME}_artifacts_${TS}"
       LOG_DIR="/root/logs_istio/${RELEASE_VERSION}_single_test/${TEST_PACKAGE}"
